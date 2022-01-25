c47244b17ab5829b839b2d6150f1ef46ca363686
Sutekh849 <danielmiller626@gmail.com>
2022-01-25 10:56:58 +0000

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+extern crate bit_vec;
+
+use std::fs;
+use std::io::{BufWriter, Write};
+use bit_vec::BitVec;
+/*
+ * This is another implementation of the Seive of Eratosthenes algorithm for 
+ * finding prime numbers, this time in rust. Mostly wrote it as a way of getting back
+ * to both programming and to rust after a long time writing Java for work.
+ */
+fn main() {
+    
+   let mut file = BufWriter::new(fs::File::create("primes.txt").unwrap());
+   let max: usize = 15485863;
+   
+   let mut bv = BitVec::from_elem(max, true);
+   
+   bv.set(0, true);
+   bv.set(1, true);
+   
+   for number in 2..max
+   {
+       if (bv.get(number)) == Some(true)
+       {
+           writeln!(file, "{}", number).expect("Failed to write to file");
+           for number2 in (number.pow(2)..max).step_by(number)
+           {
+               bv.set(number2, false);
+           }
+       }
+   }
+
+}
